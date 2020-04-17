97566fc746eb3f11cad4372877d5e2590bc2c473
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-04-17 12:43:12 -0400

    initialize PlaybllTop component

--- client/src/components/Playball.js
+++ client/src/components/Playball.js
@@ -4,10 +4,31 @@ import FieldPosition from "./FieldPosition";
 import "./Playball.scss";
 
 const Playball = ({ user, players }) => {
+  const [pitcher, setPitcher] = useState("");
+
   console.log("Palyball component user: ", user);
   console.log("Palyball component user: ", players);
   let rostNumber = 0;
   let positionNumber = 0;
+
+  // const currentPlayer = () => {
+  //   setPitcher(
+  //     players.filter(player => {
+  //       player.position === "P";
+  //     })
+  //   );
+  // };
+
+  if (players) {
+    console.log(
+      "playball players:  ",
+      players.filter(player => {
+        return player.position === "P"; // no return -> error
+      })
+    );
+  }
+  //Expected an assignment or function call and instead saw an expression  no-unused-expressions
+
   return (
     <div className="playball_body">
       <div className="playball_wrap">
